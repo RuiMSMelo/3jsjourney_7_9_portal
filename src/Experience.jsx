cee02fbb3044de06c5abdaebb2f4f1259c88ff20
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-28 17:59:28 +0100

    background

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,16 @@
 import { OrbitControls } from '@react-three/drei'
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+    return (
+        <>
+            <color args={['#030202']} attach='background' />
 
-        <OrbitControls makeDefault />
+            <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
-    </>
+            <mesh scale={1.5}>
+                <boxGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </>
+    )
 }
