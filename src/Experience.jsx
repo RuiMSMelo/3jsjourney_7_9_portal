f831e2f3be1795998150f0be07179443bbce14aa
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-28 18:36:04 +0100

    portal done

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,16 +1,77 @@
-import { OrbitControls } from '@react-three/drei'
+import {
+    shaderMaterial,
+    Sparkles,
+    Center,
+    OrbitControls,
+    useGLTF,
+    useTexture,
+} from '@react-three/drei'
+import { extend, useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+import portalVertexShader from './shaders/portal/vertex.glsl'
+import portalFragmentShader from './shaders/portal/fragment.glsl'
+import { useRef } from 'react'
+
+const PortalMaterial = shaderMaterial(
+    {
+        uTime: 0,
+        uColorStart: new THREE.Color('#ffffff'),
+        uColorEnd: new THREE.Color('#000000'),
+    },
+    portalVertexShader,
+    portalFragmentShader
+)
+extend({ PortalMaterial }) //* make a new component usable in React <PortalMaterial />
 
 export default function Experience() {
+    const { nodes } = useGLTF('./model/portal.glb')
+
+    const bakedTexture = useTexture('./model/baked.jpg')
+    bakedTexture.flipY = false
+
+    const portalMaterialRef = useRef()
+
+    useFrame((state, delta) => {
+        portalMaterialRef.current.uTime += delta
+    })
+
     return (
         <>
             <color args={['#030202']} attach='background' />
 
             <OrbitControls makeDefault />
 
-            <mesh scale={1.5}>
-                <boxGeometry />
-                <meshNormalMaterial />
-            </mesh>
+            <Center>
+                <mesh geometry={nodes.baked.geometry}>
+                    <meshBasicMaterial map={bakedTexture} />
+                </mesh>
+                <mesh
+                    geometry={nodes.poleLightA.geometry}
+                    position={nodes.poleLightA.position}
+                >
+                    <meshBasicMaterial color='#ffffe5' />
+                </mesh>
+                <mesh
+                    geometry={nodes.poleLightB.geometry}
+                    position={nodes.poleLightB.position}
+                >
+                    <meshBasicMaterial color='#ffffe5' />
+                </mesh>
+                <mesh
+                    geometry={nodes.portalLight.geometry}
+                    position={nodes.portalLight.position}
+                    rotation={nodes.portalLight.rotation}
+                >
+                    <portalMaterial ref={portalMaterialRef} />
+                </mesh>
+                <Sparkles
+                    size={4}
+                    scale={[4, 2, 4]}
+                    position-y={1}
+                    speed={0.2}
+                    count={40}
+                />
+            </Center>
         </>
     )
 }
